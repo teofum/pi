6390f75be493bb6256a8774c1b02ce9023a32717
Teo Fumagalli <teo.fum@gmail.com>
2024-03-13 10:42:47 -0300

    updated run.sh to qutodetect getnum lib use

--- run.sh
+++ run.sh
@@ -3,5 +3,15 @@
 cmd=${4:-gcc} # Use clang (aliased) on mac
 #cmd=${4:-$CC} # Use gcc on mac
 
-$cmd ./$1/$1_$2.c $3 -std=c99 -pedantic -Wall -o out/$1_$2
+src=./$1/$1_$2.c
+
+# Auto-detect use of my_getnum lib and add it to compile command
+cat $src | grep my_getnum > /dev/null
+if [[ $? ]]; then
+  $cmd $src lib/my_getnum.c $3 -std=c99 -pedantic -Wall -o out/$1_$2
+else
+  $cmd $src $3 -std=c99 -pedantic -Wall -o out/$1_$2
+fi
+
+# Run the output executable
 ./out/$1_$2
