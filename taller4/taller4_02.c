5d3571b72fa24b6a97f61cc64b478790d8ab8e35
Teo Fumagalli <teo.fum@gmail.com>
2024-04-05 15:21:16 -0300

    taller4 02

---
+++ taller4/taller4_02.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include "random.h"
+#include "utils.h"
+
+void shuffle(int vec[], int size);
+void shuffle_terminated(int vec[]);
+
+int main(void) {
+  seed_with_timestamp();
+
+  int arr[5] = {3, 5, 7, 9, 15};
+  int arr_t[6] = {3, 5, 7, 9, 15, -1};
+
+  shuffle(arr, 5);
+  shuffle_terminated(arr_t);
+
+  printf("Shuffled (length): ");
+  for (int i = 0; i < 5; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+
+  printf("Shuffled (-1-terminated): ");
+  for (int i = 0; arr_t[i] != -1; i++)
+    printf("%d ", arr_t[i]);
+  printf("\n");
+
+  return 0;
+}
+
+void shuffle(int vec[], int size) {
+  int j;
+  for (int i = 0; i < size; i++) {
+    j = random_int(0, size - 1);
+    swap(&vec[i], &vec[j]);
+  }
+}
+
+void shuffle_terminated(int vec[]) {
+  int j;
+  for (int i = 0; vec[i] != -1; i++) {
+    j = random_int(0, i);
+    swap(&vec[i], &vec[j]);
+  }
+}
