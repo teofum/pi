cbd8ed42ea3e295e659040c25e8fe765b93c69df
Teo Fumagalli <teo.fum@gmail.com>
2024-04-05 14:18:18 -0300

    update random lib

--- lib/random.c
+++ lib/random.c
@@ -1,9 +1,16 @@
 #include "random.h"
 
 void seed_with_timestamp(void) {
-  time_t t;
-  time(&t);
+  time_t t = time(NULL);
   srand(t);
 }
 
-int random_int(int min, int max) { return rand() % (max - min) + min; }
+double random() { return rand() / ((double)RAND_MAX + 1.0); }
+
+double random_range(double min, double max) {
+  return random() * (max - min) + min;
+}
+
+int random_int(int min, int max) {
+  return (int)(random() * (max - min + 1)) + min;
+}
--- lib/random.h
+++ lib/random.h
@@ -7,6 +7,12 @@
 // Seed the RNG with current time
 void seed_with_timestamp(void);
 
+// Returns a random double in range [0, 1)
+double random();
+
+// Returns a random double in range [min, max)
+double random_range(double min, double max);
+
 // Returns a random integer in range [min, max)
 int random_int(int min, int max);
 
