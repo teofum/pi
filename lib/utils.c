176d373fd1775102a5823f069f00950fb6d2aa71
Teo Fumagalli <teo.fum@gmail.com>
2024-04-19 13:07:52 -0300

    better quicksort

--- lib/utils.c
+++ lib/utils.c
@@ -13,31 +13,29 @@ void sort(int v[], int size) {
   if (size <= 1)
     return;
 
-  if (size == 2) {
-    if (v[0] > v[1])
-      swap(v, v + 1);
+  int l = 0, r = size;
+  int m = l + ((r - l) >> 1);
 
-    return;
-  }
+  int pivot = v[m];
+  v[m] = v[l];
 
-  int pivot = v[0];
-  int small = 0, big = 0;
-  int buf[size];
-
-  for (int i = 1; i < size; i++) {
-    if (v[i] < pivot) {
-      buf[small++] = v[i];
-    } else {
-      buf[size - 1 - (big++)] = v[i];
+  while (l < r) {
+    while (v[r] >= pivot && l < r) {
+      r--;
+    }
+    if (l < r) {
+      v[l++] = v[r];
+    }
+    while (v[l] <= pivot && l < r) {
+      l++;
+    }
+    if (l < r) {
+      v[r--] = v[l];
     }
   }
+  v[l] = pivot;
+  m = l + 1;
 
-  buf[small] = pivot;
-
-  sort(buf, small);
-  sort(buf + small + 1, big);
-
-  for (int i = 0; i < size; i++) {
-    v[i] = buf[i];
-  }
+  sort(v, l);
+  sort(v + m, size - m);
 }
