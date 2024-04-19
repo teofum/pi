5f124d936f8d2e7346ba8f1611cdf936260a7cee
Teo Fumagalli <teo.fum@gmail.com>
2024-04-19 12:47:08 -0300

    quicksort util

--- lib/utils.c
+++ lib/utils.c
@@ -1,7 +1,43 @@
+#include <stdio.h>
+
 #include "utils.h"
 
 void swap(int *a, int *b) {
   int tmp = *a;
   *a = *b;
   *b = tmp;
+}
+
+// quicksort
+void sort(int v[], int size) {
+  if (size <= 1)
+    return;
+
+  if (size == 2) {
+    if (v[0] > v[1])
+      swap(v, v + 1);
+
+    return;
+  }
+
+  int pivot = v[0];
+  int small = 0, big = 0;
+  int buf[size];
+
+  for (int i = 1; i < size; i++) {
+    if (v[i] < pivot) {
+      buf[small++] = v[i];
+    } else {
+      buf[size - 1 - (big++)] = v[i];
+    }
+  }
+
+  buf[small] = pivot;
+
+  sort(buf, small);
+  sort(buf + small + 1, big);
+
+  for (int i = 0; i < size; i++) {
+    v[i] = buf[i];
+  }
 }
--- lib/utils.h
+++ lib/utils.h
@@ -6,4 +6,7 @@
 // Swaps the values of two integers
 void swap(int *a, int *b);
 
+// Sort an integer array using quicksort
+void sort(int v[], int size);
+
 #endif
