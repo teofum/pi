46b6f55578e59c43c448eed7de4cf1eb175aa780
Teo Fumagalli <teo.fum@gmail.com>
2024-04-23 11:25:46 -0300

    clion setup

---
+++ make_cmakelists.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+exclude_dirs=(".vscode" "cmake-build-debug" "lib" "out")
+
+echo "# Auto generated CMakeLists.txt" > CMakeLists.txt
+
+while read line; do
+  echo $line >> CMakeLists.txt
+done < CMakeLists.template.txt
+
+for dir in */; do
+  dir=${dir%*/}
+
+  # Iterate each directory not in exclude_dirs
+  if [[ ! " ${exclude_dirs[*]} " =~ [[:space:]]${dir}[[:space:]] ]]; then
+    echo "" >> CMakeLists.txt
+    echo "# $dir/" >> CMakeLists.txt
+
+    for file in $dir/*.c; do
+      # Auto-detect use of libs and add it to compile command
+      out=$(basename -- "$file")
+      out=${out%.*}
+      src=$file
+      for lib in lib/*.h; do
+        lib_name=$(echo $lib | sed -E 's#lib/([a-zA-Z0-9_]+)\.h#\1#')
+        cat $src | grep "$lib_name.h" > /dev/null
+        if [[ $? = 0 ]]; then
+          src="$src lib/$lib_name.c"
+        fi
+      done
+
+      echo "add_executable($out $src)" >> CMakeLists.txt
+    done
+  fi
+done
+
