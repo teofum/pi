883a56cec1359607feccf4f889b2d0fe97ca1872
Teo Fumagalli <teo.fum@gmail.com>
2024-04-23 09:10:22 -0300

    tp7 01, add sanitize option to run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ cmd=${4:-gcc} # Use clang (aliased) on mac
 #cmd=${4:-$CC} # Use gcc on mac (needs envvar set in .zshrc)
 
 src=./$1/$1_$2.c
-args="$3 -I ./lib -std=c99 -pedantic -Wall -lm -o out/$1_$2"
+args="-I ./lib -std=c99 -pedantic -Wall -lm -o out/$1_$2"
+
+if [[ $3 == "san" ]]; then
+  args="$args -fsanitize=address -g"
+else
+  args="$args $3"
+fi
 
 # Auto-detect use of libs and add it to compile command
 for lib in lib/*.h; do
